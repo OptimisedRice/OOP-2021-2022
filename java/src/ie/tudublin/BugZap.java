8a201b85a1ab71b2309e8ddcdb36714a4bb317e8
C20483514 Kieran Retardo Silada <c20483514@mytudublin.ie>
2022-02-03 09:41:45 +0000

    bug moving

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -6,6 +6,8 @@ public class BugZap extends PApplet
 {
 	float playerX, playerY, playerwidth;
 
+	float bugX, bugY, bugwidth;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -19,6 +21,23 @@ public class BugZap extends PApplet
 		playerY = height - 50;
 		playerwidth = 50;
 
+		resetBug();
+
+	}
+
+	private void resetBug()
+	{
+		bugX = random(bugwidth / 2, width - (bugwidth / 2));
+		bugY = 50;
+		bugwidth = 50;
+	}
+
+	void drawBug(float x, float y, float w)
+	{
+		float halfw = w/2;
+		stroke(255);
+		noFill();
+		triangle(x - halfw, y + halfw, x, y - halfw, x + halfw, y + halfw);
 	}
 
 	void drawPlayer(float x, float y, float w)
@@ -30,11 +49,37 @@ public class BugZap extends PApplet
 		line(x,y - 10, x, y-20);
 
 	}
+
+	float playerSpeed = 5;
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			playerX -= playerSpeed;
+		}
+		if (keyCode == RIGHT)
+		{
+			playerX += playerSpeed;
+		}
+	}
+
+	void moveBug()
+	{
+		bugY ++;
+		bugX += random (-20, 20);
+	}
 	
 	public void draw()
 	{	
 		background(0);
 		strokeWeight(2);
 		drawPlayer(playerX, playerY, playerwidth);
+		drawBug(bugX, bugY, bugwidth);
+		if (frameCount % 20 == 0)
+		{
+			moveBug();
+		}
+		
 	}
 }
