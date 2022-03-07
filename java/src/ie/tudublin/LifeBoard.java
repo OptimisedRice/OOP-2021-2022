67555503910c708a2247ce7ade3cd5e27a6c8143
C20483514 Kieran Retardo Silada <c20483514@mytudublin.ie>
2022-03-07 11:49:04 +0000

    number1

---
+++ java/src/ie/tudublin/LifeBoard.java
@@ -0,0 +1,38 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class LifeBoard {
+    boolean[][] board;
+
+    int size;
+    int cellSize;
+    PApplet pa;
+
+    public LifeBoard(int size, PApplet pa)
+    {
+        board = new boolean[size][size];
+        this.size = size;
+        this.pa = pa;
+        cellSize = pa.width / (float) size;
+    }
+
+    public void randomise()
+    {
+        for(int row = 0; row < size; row++)
+        {
+            for(int col = 0; col < size; col++)
+            {
+                board[row][col] = pa.random(1.0f) > 0.5f;
+            }
+        }
+    }
+
+    public void render()
+    {
+        
+    }
+
+}
+
+
